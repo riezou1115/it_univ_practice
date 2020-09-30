21bfb9cad4202bf7530363e91bee490c65809705
riezou1115 <72135917+riezou1115@users.noreply.github.com>
2020-09-30 20:16:51 +0900

    initial commit
    初めてアップしました。

---
+++ ruby_main.rb
@@ -0,0 +1,16 @@
+answer = rand(10) + 1
+count = 0
+loop do
+  print "Your guess? "
+  guess = gets.to_i
+  count +=1
+
+  if answer == guess
+    puts "Bingo! It took #{count} guesses!"
+    break
+  elsif answer > guess
+    puts "Bigger!"
+  else
+    puts "Smaller!"
+  end
+end
